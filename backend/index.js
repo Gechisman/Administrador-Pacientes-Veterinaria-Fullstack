a41b6b3a8200117bbee2d262004ba18a274e7009
Gechisman <rodrigo.pinelpastrana@gmail.com>
2025-01-15 16:03:20 +0100

    Creación de cuenta con verificaciones

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import conectarDB from './config/db.js';
 import dotenv from "dotenv";
+import cors from 'cors' //Protege una API (solo permite peticiones de la url que yo permita)
 import veterinarioRoutes from './routes/veterinarioRoutes.js';
 import pacienteRoutes from './routes/pacienteRoutes.js';
 
@@ -11,6 +12,20 @@ dotenv.config();
 
 conectarDB();
 
+const dominiosPermitidos = ['http://localhost:5173']
+
+const corsOptions = {
+    origin: function(origin, callback){
+        if (dominiosPermitidos.indexOf(origin) !== -1) {
+            //El origen del request está permitido
+            callback(null, true) //No hay mensaje de error y permite el acceso
+        } else {
+            callback( new Error('No permitido por CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 app.use('/api/veterinarios', veterinarioRoutes);
 app.use('/api/pacientes', pacienteRoutes);
 
--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from 'react-router-dom';
+import axios from "axios"
 import Alerta from "../components/Alerta";
 
 function Registrar() {
@@ -10,7 +11,7 @@ function Registrar() {
 
     const [alerta, setAlerta] = useState({})
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
       e.preventDefault()
       
       if ([nombre, email, password, repetirPassword].includes('')) {
@@ -28,9 +29,25 @@ function Registrar() {
         return;
       }
 
+      //Limpiar alerta
       setAlerta({})
 
       //Crear el usuario en la API
+      try {
+        //axios Por defecto es get
+        const url = "http://localhost:4000/api/veterinarios"
+        await axios.post(url, {nombre, email, password})
+        setAlerta({
+          msg: 'Creado correctamente, revisa tu correo', 
+          error: false
+        });
+      } catch (error) {
+        setAlerta({ 
+          msg: error.response.data.msg, 
+          error: true
+        });
+      }
+
     }
 
     const { msg } = alerta
